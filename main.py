db564a8a5ba11ed1cb36f5724b19d8e5b1ec846f
Wesley Pereira <>
2022-08-04 14:04:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def mult(a ,b):
+    
+    m = (a * b)
+    print(m)
+   
+n1 = float(input("digite o primeiro número: "))
+n2 = float(input("digite o segundo número: "))
+   
+mult(n1, n2)          
