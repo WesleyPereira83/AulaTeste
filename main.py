7c56de32ced036235c69163aa04e306366a83096
WesleyPereira83 <110615423+WesleyPereira83@users.noreply.github.com>
2022-08-04 11:09:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def mult(a ,b):
     print(m)
    
 n1 = float(input("digite o primeiro número: "))
-n2 = float(input("digite o segundo número: "))
+n2 = float(input("digite o seu segundo número: "))
    
 mult(n1, n2)          
